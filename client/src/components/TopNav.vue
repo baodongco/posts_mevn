5a39a65ce71156c763593c8c9029efc7a360a8f7
baodongco <blog.vip.uel@gmail.com>
2018-08-27 02:11:01 +0700

    update server setting

---
+++ client/src/components/TopNav.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="top-nav">
+    <header id="header">
+		<div id="banner">
+            <div class="margin_auto">
+                <div id="logo">
+                    <a href="index.html">
+                        <img src="upload/hinhanh/thoitrangbexinh-409_210x103.png" width="100%" alt="Thời trang bé xinh">
+                    </a>
+                </div>
+                <div class="hotline_top">
+                    <p> Hotline: <span> 0908 880 059 </span> </p> 
+                </div>
+                <div class="slogan">
+                    <marquee>Chuyên cung cấp sỉ quần áo trẻ em Việt Nam xuất khẩu</marquee>
+                </div>  
+            </div>
+            <div class="hotrotop">
+                    <div class="htt">
+                <a href="ho-tro-khach-hang/thanh-toan.html"><img src="upload/baiviet/3-8926.png" alt="Thanh toán">
+                    <h3>Thanh toán</h3>
+                </a>
+            </div>
+                    <div class="htt">
+                <a href="ho-tro-khach-hang/dat-hang.html"><img src="upload/baiviet/2-5729.png" alt="Đặt hàng">
+                    <h3>Đặt hàng</h3>
+                </a>
+            </div>
+                    <div class="htt">
+                <a href="ho-tro-khach-hang/van-chuyen.html"><img src="upload/baiviet/4-3655.png" alt="Vận chuyển">
+                    <h3>Vận chuyển</h3>
+                </a>
+            </div>
+                    <div class="htt">
+                <a href="ho-tro-khach-hang/doi-hang.html"><img src="upload/baiviet/doitrahang-3270.jpg" alt="Đổi hàng">
+                    <h3>Đổi hàng</h3>
+                </a>
+            </div>
+        </div>
+</div>
+		</header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopNav",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#header {
+    background: #fff;
+    float: left;
+    width: 100%;
+    margin: 0 auto;
+    z-index: 9;
+    position: relative;
+    max-width: 1366px;
+}
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,6 +4,7 @@ import HelloWorld from '@/components/HelloWorld'
 import Posts from '@/components/Posts'
 import NewPost from '@/components/NewPost'
 import EditPost from '@/components/EditPost'
+import TopNav from '@/components/TopNav'
 
 Vue.use(Router)
 
@@ -28,6 +29,11 @@ export default new Router({
       path: '/posts/:id',
       name: 'EditPost',
       component: EditPost
+    },
+    {
+      path: '/topnav',
+      name: 'TopNav',
+      component: TopNav
     }
   ]
 })
